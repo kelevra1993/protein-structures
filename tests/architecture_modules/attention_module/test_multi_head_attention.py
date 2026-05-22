ac37c16f1a6fb51568caed7a99a72e0c12b2e153
Robert Kelevra DGX Spark <kelevra.robert@gmail.com>
2026-05-22 19:56:55 +0200

    [Test] Simplify tests

--- tests/architecture_modules/attention_module/test_multi_head_attention.py
+++ tests/architecture_modules/attention_module/test_multi_head_attention.py
@@ -1,6 +1,10 @@
 import math
 import torch
 from torch import nn
+from pathlib import Path
+
+from architecture_modules.attention_module.multi_head_attention import MultiHeadAttention
+from tests.utilities.testing_utilities import test_nn_module_method
 
 batch_size = 2
 input_dimension = 8
@@ -13,40 +17,18 @@ dimension_3 = 6
 dimension_4 = 7
 
 feature_shapes = {
-    'x': (dimension_1, dimension_2, dimension_3, dimension_4, input_dimension),
-    'bias': (number_heads, dimension_3, dimension_3),
-    'fake_attention_mask': (dimension_1, dimension_2, dimension_4, dimension_3),
-    'attention_mask': (128,),
+    'input_tensor': (dimension_1, dimension_2, dimension_3, dimension_4, input_dimension),
+    'bias_tensor': (number_heads, dimension_3, dimension_3),
+    'attention_mask_tensor': (128,),
 }
 
-batched_feature_shapes = {
-    key: (batch_size,) + value
-    for key, value in feature_shapes.items()
-}
 
 test_inputs = {
     key: torch.linspace(-2 - i / 5, 2 + i / 5, math.prod(shape)).reshape(shape).double()
     for i, (key, shape) in enumerate(feature_shapes.items())
 }
 
-test_inputs['fake_attention_mask'] = torch.ones((dimension_1, dimension_2, dimension_4, dimension_3))
-test_inputs['fake_attention_mask'][..., 2:4] = 0
-
-test_inputs['input_ids'] = torch.tensor([101, 5342, 2047, 3595, 8496, 2013, 1996, 18643, 3197, 102,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0])
-
-test_inputs['attention_mask'] = torch.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+test_inputs['attention_mask_tensor'] = torch.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
