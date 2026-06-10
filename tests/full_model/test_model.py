2014b6eabda507d1a44fdb43f5ecc8cb1d7d1aed
Robert Kelevra <kelevra.robert@gmail.com>
2026-06-10 08:15:08 +0200

    [TestModel] Minor Update

--- tests/full_model/test_model.py
+++ tests/full_model/test_model.py
@@ -1,52 +1,49 @@
+import os
 import math
 import torch
 from torch import nn
 from pathlib import Path
 from tests.utilities.testing_utilities import check_nn_module_method
 from full_model.model import Model
-
-number_cycles = 2
-number_evoformer_blocks = 3
-number_extra_msa_blocks = 4
-batch_size = 5
-msa_embedding = 6
-pair_representation_embedding = 7
-head_embedding_dimension = 8
-number_heads = 9
-number_clusters = 10
-number_extra_sequences = 11
-number_residues = 12
-single_representation_embedding = 384
-number_query_points = 14
-number_value_points = 15
-number_torsion_angles = 7
-input_sequence_feature_dimension = 21
-msa_feature_dimension = 49
-input_extra_msa_feature_dimension = 18
-extra_msa_embedding = 19
-
-feature_shapes = {
-    'input_msa_feature': (number_clusters, number_residues, msa_feature_dimension, number_cycles),
-    'input_sequence_feature': (number_residues, input_sequence_feature_dimension, number_cycles),
-    'input_residue_index_feature': (number_residues, number_cycles),
-    'input_extra_msa_feature': (number_extra_sequences, number_residues, input_extra_msa_feature_dimension,
-                                number_cycles),
-}
-
-test_inputs = {
-    key: torch.linspace(-2 - i / 5, 2 + i / 5, math.prod(shape)).reshape(shape).double()
-    for i, (key, shape) in enumerate(feature_shapes.items())
-}
-
-test_inputs['input_residue_index_feature'] = torch.arange(number_residues).view(number_residues, 1).broadcast_to(
-    number_residues, number_cycles)
-test_inputs['input_sequence_feature'] = nn.functional.one_hot(torch.arange(number_residues) % 20,
-                                                              num_classes=input_sequence_feature_dimension).double()
-test_inputs['input_sequence_feature'] = test_inputs['input_sequence_feature'].unsqueeze(-1).broadcast_to(
-    feature_shapes['input_sequence_feature'])
+from utilities.data.input import ModelInput
 
 
 def test_model():
+    number_cycles = 2
+    number_evoformer_blocks = 3
+    number_extra_msa_blocks = 4
+    batch_size = 3
+    msa_embedding = 6
+    pair_representation_embedding = 7
+    number_clusters = 10
+    number_extra_sequences = 11
+    single_representation_embedding = 384
+    input_sequence_feature_dimension = 21
+    msa_feature_dimension = 49
+    input_extra_msa_feature_dimension = 25
+    extra_msa_embedding = 19
+    number_residues = 64  # Use a crop size
+
+    device = torch.device('cpu')
+    dtype = torch.float64
+    reference_folder = os.path.join(os.path.dirname(__file__), 'reference_values')
+
+    protein_id = "P90561"
+    structure_path = os.path.join(reference_folder, f"structures/{protein_id}.npz")
+    msa_path = os.path.join(reference_folder, f"raw_msa/{protein_id}.a3m")
+    record_path = os.path.join(reference_folder, f"records/{protein_id}.json")
+
+    model_input = ModelInput(structure_path=str(structure_path), msa_path=str(msa_path), record_path=str(record_path),
+                             acceptance_slope_start=256, acceptance_slope_end=512,
+                             residue_crop_size=None, emphasize_beginning_crops=False,
+                             distribution_threshold=100,
+                             maximum_cluster_sequences=number_clusters,
+                             maximum_extra_msa_sequences=number_extra_sequences,
+                             mask_probability=0.0,
+                             device=device, dtype=dtype)
+
+    data = model_input.get_data(number_samples=number_cycles, seed=42)
+
     configuration = {
         'GlobalConfiguration': {
             'msa_embedding': msa_embedding,
@@ -96,19 +93,43 @@ def test_model():
     }
     model = Model(
         configuration=configuration,
-        device=torch.device('cpu'),
-        dtype=torch.float64
+        device=device,
+        dtype=dtype
     )
+
+    simple_input_dict = {
+        'input_msa_feature': data['input_msa_feature'][:, :number_residues, ...].unsqueeze(0),
+        'input_extra_msa_feature': data['input_extra_msa_feature'][:, :number_residues, ...].unsqueeze(0),
+        'input_sequence_feature': data['input_sequence_feature'][:number_residues, ...].unsqueeze(0),
+        'input_residue_index_feature': data['input_residue_index_feature'][:number_residues, ...].unsqueeze(0),
+        'ground_truth_transformation_matrix': data['ground_truth_frames'][:number_residues, ...].unsqueeze(0),
+        'alternative_ground_truth_transformation_matrix': data['alternative_ground_truth_frames'][
+            :number_residues, ...].unsqueeze(0),
+        'ground_truth_angles': data['ground_truth_angles'][:number_residues, ...].unsqueeze(0),
+        'alternative_ground_truth_angles': data['alternative_ground_truth_angles'][:number_residues, ...].unsqueeze(0),
+        'ground_truth_positions': data['ground_truth_global_positions'][:number_residues, ...].unsqueeze(0),
+        'alternative_ground_truth_positions': data['alternative_ground_truth_global_positions'][
+            :number_residues, ...].unsqueeze(0),
+    }
+
+    batched_input_dict = {
+        key: tensor.repeat(batch_size, *([1] * len(tensor.shape[1:]))) for key, tensor in simple_input_dict.items()
+    }
 
     check_nn_module_method(
         module=model,
-        input_tensor_dictionary=test_inputs,
+        input_tensor_dictionary=simple_input_dict,
+        batched_input_tensor_dictionary=batched_input_dict,
         output_tensor_names=["model_angles",
                              "model_frames",
                              "model_final_positions",
                              "model_position_mask",
-                             "model_pseudo_beta_positions"],
-        reference_folder=Path(__file__).parent / "reference_values",
+                             "model_pseudo_beta_positions",
+                             "model_overall_fape_loss",
+                             "model_auxillary_loss",
+                             "model_predicted_lddt_loss",
+                             "model_distogram_logits"],
+        reference_folder=reference_folder,
         batch_size=batch_size,
         use_kwargs=False)
     print("Full Model Test Completed Successfully.")
