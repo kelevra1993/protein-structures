de4ffe5f7a70aae57a4562ef5987544d484ff833
Robert Kelevra <kelevra.robert@gmail.com>
2026-06-13 15:36:36 +0200

    [Visualizer] Globalise path and only use matplotlib.

--- utilities/debugger/run_frame_visualizer.py
+++ utilities/debugger/run_frame_visualizer.py
@@ -7,6 +7,8 @@ import torch
 import os
 from utilities.data.structure import Structure
 from utilities.debugger.visualization_utilities import visualization
+from pathlib import Path
+from utilities.os_utilities import globalise_path
 
 DEFAULT_NPZ = "data_examples/openfold/structures/P90561.npz"
 
@@ -17,15 +19,15 @@ def main():
                         help=f"Path to the .npz structure file (default: {DEFAULT_NPZ}).")
     parser.add_argument("--record", type=str,
                         help="Path to the corresponding .json record file (optional).")
-    parser.add_argument("--num_residues", type=int, default=50,
+    parser.add_argument("--num_residues", type=int, default=60,
                         help="Number of residues to visualize.")
-    parser.add_argument("--method", type=str, choices=["matplotlib", "plotly"],
-                        default="matplotlib",
-                        help="Visualization method to use.")
     parser.add_argument("--frame_index", type=int, default=0,
                         help="Index of the frame to visualize (0 for backbone).")
 
     args = parser.parse_args()
+
+    project_path = Path(os.getcwd()).parents[1]
+    args.npz = str(globalise_path(absolute_parent_path=project_path, target_path=Path(args.npz)))
 
     if not os.path.exists(args.npz):
         print(f"Error: File {args.npz} not found.")
@@ -46,8 +48,8 @@ def main():
     num_to_show = min(args.num_residues, selected_frames.shape[0])
     frames_to_visualize = selected_frames[:num_to_show]
 
-    print(f"Visualizing {num_to_show} residues (Frame {args.frame_index}) using {args.method}...")
-    visualization(method=args.method, frames_tensor=frames_to_visualize)
+    print(f"Visualizing {num_to_show} residues (Frame {args.frame_index}) using Matplotlib...")
+    visualization(frames_tensor=frames_to_visualize)
 
 
 if __name__ == "__main__":
